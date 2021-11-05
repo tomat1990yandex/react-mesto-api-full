58ec760c3f18c14a44164d2969ef97491a951600
Dmitriy Orlov <tomat1990@yandex.ru>
2021-11-06 01:08:29 +0500

    fix owner

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,13 +5,13 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
 
   const currentUser = useContext(CurrentUserContext);
 
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
 
   const cardDeleteButtonClassName = (
     `element__delete ${isOwn ? 'element__delete_visible' : ''}`
   );
 
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i === currentUser._id);
 
   const cardLikeButtonClassName = (
     `element__like ${isLiked ? 'element__like_active' : ''}`
